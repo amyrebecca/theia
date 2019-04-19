67482bd5fc1900910511e2abdff9e6115d8b696e
Amy Boyer <amy@zooniverse.org>
2019-04-19 15:07:09 -0700

    authenticates and can confirm logged in

--- app/tasks.py
+++ app/tasks.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from app.usgs import ErosWrapper
 
 app = Celery('tasks', broker='redis://redis', backend='redis://redis')
 
@@ -6,3 +7,8 @@ app = Celery('tasks', broker='redis://redis', backend='redis://redis')
 @app.task(name='tasks.add')
 def add(x, y):
     return x+y
+
+
+@app.task(name='tasks.locate_scenes')
+def locate_scenes(imagery_request_id):
+    return
